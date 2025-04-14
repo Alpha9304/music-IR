f53ef7b90c7b8cbcb34a42dedaa9eb38e20ccce7
Alpha9304 <gbemisoladosu@gmail.com>
2025-04-14 18:28:49 -0400

    my computer is scaring me so I'm going to push

--- process_music.py
+++ process_music.py
@@ -1,6 +1,10 @@
 import sys
 import partitura as pt
 
+#convert (accidentals, mode) to a unique single digit
+key_sig_map = {(0, 0): 0, (0, 1): 1, (1, 0): 2, (1, 1): 3, (-1, 0): 4, (-1, 1): 5, (2, 0): 6, (2, 1): 7, (-2, 0): 8, (-2, 1): 9, (3, 0): 10, (3, 1): 11, (-3, 0): 12, (-3, 1): 13, 
+               (4, 0): 14, (4, 1): 15, (-4, 0): 16, (-4, 1): 17, (5, 0): 18, (5, 1): 19, (-5, 0): 20, (-5, 1): 21, (6, 0): 22, (6, 1): 23, (-6, 0): 24, (-6, 1): 25, (7, 0): 26, 
+               (7, 1): 27, (-7, 0): 28, (-7, 1): 29}
 def read_data(file):
     '''
     Reads the data
@@ -98,31 +102,55 @@ def test_out_library(score): #note in midis some of this data may be missing and
         measures = part.measures
         print(len(measures))
 
-    #print("Compute Note Array of Part with Some information:") #format is (...default info, note ID, pitch spelling letter, accidental (0 = flat, 1 = sharp?), octave num, ''(if grace nots), key sig num 1 (accidentals), key sig num 2 (mode), time_sig num, time_sig denom, beats)
+    print("Compute Note Array of Part with Some information:") #format is (...default info, note ID, pitch spelling letter, accidental (0 = flat, 1 = sharp?), octave num, ''(if grace nots), key sig num 1 (accidentals), key sig num 2 (mode), time_sig num, time_sig denom, beats)
     #use this, lots of info and better formed than straight time sign estimate...
-    #for part in parts:
-    #    print(pt.musicanalysis.compute_note_array(part, include_pitch_spelling = True, include_key_signature = True))#, include_time_signature = True, include_grace_notes = True))
+    for part in parts:
+        print(pt.musicanalysis.compute_note_array(part, include_pitch_spelling = True, include_key_signature = True))#, include_time_signature = True, include_grace_notes = True))
 
 def extract_features(score):
     parts = score.parts
 
     #get number of parts
     num_parts = len(parts)
+    print("number of instruments", num_parts)
 
     #get tempo
 
     key_counts = {}
+    time_sig_counts = {}
     #get most frequent/main key signature
     for part in parts:
-        note_array = print(pt.musicanalysis.compute_note_array(part, include_key_signature = True))
+        note_array = pt.musicanalysis.compute_note_array(part, include_key_signature = True, include_time_signature = True)
         for note in note_array:
+            #key signature information
             key_sig_accidentals = note[9]
             key_sig_mode = note[10] #how to translate this to a numeric identifier?; can't just sum them...
+            key_sig_code = key_sig_map[(key_sig_accidentals, key_sig_mode)]
+            if key_sig_code in key_counts:
+                key_counts[key_sig_code] += 1
+            else:
+                key_counts[key_sig_code] = 1
+
+            #time signature information
+            time_sig_num = note[11]
+            time_sig_denom = note[12]
+            time_sig_code = hash((note[11], note[12]))
+            if time_sig_code in time_sig_counts:
+                time_sig_counts[time_sig_code] += 1
+            else:
+                time_sig_counts[time_sig_code] = 1
+
+    most_frequent_key = max(key_counts, key=key_counts.get)
+    print("most frequent key code:", most_frequent_key)
 
     #get number of key_changes/key signatures
+    num_key_signatures = len(key_counts)
+    print("num key signatures: ", num_key_signatures)
 
     #get number of time signatures
-
+    print(time_sig_counts)
+    num_time_signatures = len(time_sig_counts)
+    print("num time signatures", num_time_signatures)
     #get instrument names or maybe just num instruments?
 
     #get dynamics
@@ -131,7 +159,8 @@ def extract_features(score):
     num_rests = 0
     for part in parts:
         rests = part.rests
-        num_rests += rests
+        num_rests += len(rests)
+    print("total rests: " , num_rests)
 
     #get average number of measures per part
     total_measures = 0
@@ -140,13 +169,15 @@ def extract_features(score):
         total_measures += len(measures)
 
     avg_measures = total_measures/num_parts
+    print("avg measures per  part: ", avg_measures)
 
     
 
 def main(): #take this away later so this file can just be run by the system
     input_filename = sys.argv[1]
     input_score = pt.load_score(input_filename) #this means the UI will have to take in the uploaded file and put it in the system to load it; and then delete it after
-    test_out_library(input_score)
+    #test_out_library(input_score)
+    extract_features(input_score)
     #print(parts)
     #print(input_score_parts.key_signature_map(input_score_parts.notes[0].start.ts))
     #
